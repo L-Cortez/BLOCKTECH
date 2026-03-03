9eefcdf1eabd57aebb0e3a5b6adfc282cbe00355
L-Cortez <cortezlawrence28@gmail.com>
2026-03-03 14:05:38 +0800

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+export default {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
+};
