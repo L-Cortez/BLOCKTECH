eaa9ebc9348a6241a58c91aec3a09c598a9cfd49
L-Cortez <cortezlawrence28@gmail.com>
2026-03-03 13:22:09 +0800

    Add files via upload

---
+++ Finals-Lab-Act3/SimpleGoldVault-Cortez.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SimpleGoldVault is ERC4626 {
+
+    constructor(ERC20 _asset)
+        ERC20("Vault Gold Shares", "vGOLD")
+        ERC4626(_asset)
+    {}
+}
